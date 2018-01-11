bcc3bda55521f42b3dd22cfed3b40bcd93fcf9fb
morganmclean <morgan_mclean@hotmail.com>
2018-01-10 20:37:39 -0800

    Fixed error bug again

--- api/cart/index.js
+++ api/cart/index.js
@@ -109,6 +109,41 @@
           });
       },3000);
     }
+    else
+    {
+        async.waterfall([
+            function (callback) {
+                request(endpoints.catalogueUrl + "/catalogue/" + req.body.id.toString(), function (error, response, body) {
+                    console.log(body);
+                    callback(error, JSON.parse(body));
+                });
+            },
+            function (item, callback) {
+                var options = {
+                    uri: endpoints.cartsUrl + "/" + custId + "/items",
+                    method: 'POST',
+                    json: true,
+                    body: {itemId: item.id, unitPrice: item.price}
+                };
+                console.log("POST to carts: " + options.uri + " body: " + JSON.stringify(options.body));
+                request(options, function (error, response, body) {
+                    if (error) {
+                        callback(error)
+                        return;
+                    }
+                    callback(null, response.statusCode);
+                });
+            }
+        ], function (err, statusCode) {
+            if (err) {
+                return next(err);
+            }
+            if (statusCode != 201) {
+                return next(new Error("Unable to add to cart. Status code: " + statusCode))
+            }
+            helpers.respondStatus(res, statusCode);
+        });
+    }
   });
 
 // Update cart item
